7e29d55a47f45f2be1f66d72786374cf1afdd082
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2019-11-06 15:37:33 -0300

    Floating Action Button which toggles document.designMode on/off (development environment only)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,10 @@ function random_number(first, last) {
 
 $(document).ready(function(){
     $('body').bootstrapMaterialDesign();
+
+    $('#fab').click(function(){
+        var enterDesignMode = ($('#fab button i').text() == "visibility");
+        $('#fab button i').text(enterDesignMode ? "edit" : "visibility");
+        document.designMode = enterDesignMode ? "on" : "off";
+    });
 });
